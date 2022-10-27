60aa5027b41b85f2a9429760e04b4355ada75a14
Luca Pireddu <pireddu@crs4.it>
2022-10-27 09:24:51 +0200

    Manage minio docker-compose as a test fixture

--- .tests/conftest.py
+++ .tests/conftest.py
@@ -1,10 +1,13 @@
 
 import logging
+import os
 import shutil
+import subprocess as sp
+import yaml
 
 from copy import deepcopy
 from pathlib import Path
-from tempfile import TemporaryDirectory
+from tempfile import NamedTemporaryFile, TemporaryDirectory
 from typing import Any, Dict, Generator
 
 import pytest
@@ -15,13 +18,55 @@ import helpers
 _log = logging.getLogger('fair-crcc-get-data.tests')
 
 
-@pytest.fixture
-def s3_connection() -> Dict[str, Any]:
-    return dict(
+@pytest.fixture(scope='session')
+def minio() -> Generator[Dict[str, Any], None, None]:
+    docker_compose_exec = shutil.which("docker-compose")
+    if not docker_compose_exec:
+        raise RuntimeError("Could not find docker-compose executable in PATH")
+
+    connection_info = dict(
         access_key_id="crc-user",
         secret_access_key="crc-user-s3cr3t",
         host="http://localhost:9000",
         verify=False)
+
+    minio_storage_dir = helpers.local_object_space()
+    docker_compose = helpers.minio_create_docker_compose_def(connection_info, minio_storage_dir)
+
+    with NamedTemporaryFile(mode="w", prefix="wf_test_docker-compose", suffix=".yml") as dc_file:
+        yaml.safe_dump(docker_compose, dc_file)
+        dc_file.flush()
+
+        if _log.isEnabledFor(logging.DEBUG):
+            with open(dc_file.name) as f:
+                _log.debug("Minio docker-compose file:\n%s", f.read())
+
+        # Ensure the storage
+        minio_storage_dir.mkdir(parents=True, exist_ok=True)
+        cmd = [docker_compose_exec, "-f", dc_file.name, "up", "-d"]
+        _log.info("Launching minio docker-compose")
+        sp.check_call(cmd)
+
+        try:
+            _log.info("Waiting for minio to start...")
+            if not helpers.minio_wait(connection_info['host'], num_retries=10, sleep_time=5):
+                raise RuntimeError("Minio did not come up. Aborting")
+
+            yield connection_info
+        finally:
+            try:
+                # bring down the docker-compose
+                _log.info("Bringing down minio...")
+                sp.check_call([docker_compose_exec, "-f", dc_file.name, "down", "-v"])
+                _log.info("minio shut down.")
+            except sp.CalledProcessError as e:
+                _log.error("Exception raised while trying to bring down minio docker-compose")
+                _log.exception(e)
+
+
+@pytest.fixture
+def s3_connection(minio) -> Dict[str, Any]:
+    return deepcopy(minio)
 
 
 @pytest.fixture
--- .tests/helpers.py
+++ .tests/helpers.py
@@ -3,10 +3,14 @@ import logging
 import os
 import shutil
 import subprocess as sp
+import time
 
 from pathlib import Path
 from tempfile import NamedTemporaryFile, TemporaryDirectory
 from typing import Any, Dict
+from urllib.parse import urlparse
+from urllib.request import urlopen
+from urllib.error import URLError
 
 import yaml
 
@@ -57,7 +61,6 @@ def run_workflow(wf_config: Dict[str, Any], run_directory: Path) -> None:
         "--show-failed-logs",
         "--use-singularity",
         "--singularity-prefix", str(get_img_cache()),
-        "--verbose",
         "--snakefile", str(Path(repo_root(), "workflow/Snakefile").absolute()),
     ]
     snakemake_exec = shutil.which("snakemake")
@@ -90,3 +93,66 @@ def run_workflow(wf_config: Dict[str, Any], run_directory: Path) -> None:
                     with open(item) as logfile:
                         _log.error(logfile.read() or "<file was empty>")
             raise
+
+
+def minio_create_docker_compose_def(connection_info: Dict[str, Any], minio_storage_dir: Path) -> Dict[str, Any]:
+    docker_compose = {
+        'version': '3',
+        'services': {
+            'minio': {
+                # This is the latest minio RELEASE of minio allowing us to access S3 data
+                # directly through the underlying file system (which is mounted in the
+                # container). The minio discontinued that feature in June 2022.  If an upgrade
+                # is forced in the future, we can either reimplement the tests to treat the
+                # file system storage space as opaque or replace minio with other options.
+                'image': 'minio/minio:RELEASE.2022-05-26T05-48-41Z.fips',
+                'ports': ['9000:9000', '9001:9001'],
+                'environment': [
+                    "MINIO_BROWSER=on",
+                    "MINIO_HTTP_TRACE=/dev/stderr",
+                    f"MINIO_ROOT_USER={connection_info['access_key_id']}",
+                    f"MINIO_ROOT_PASSWORD={connection_info['secret_access_key']}",
+                    f"AWS_ACCESS_KEY_ID={connection_info['access_key_id']}",
+                    f"AWS_SECRET_ACCESS_KEY={connection_info['secret_access_key']}",
+                    "AWS_DEFAULT_REGION=",
+                    f"AWS_S3_ENDPOINT={urlparse(connection_info['host']).netloc}",
+                    f"STORAGE_BUCKET={destination_root_path().parts[0]}"
+                ],
+                'entrypoint': [
+                    '/bin/sh',
+                    '-c',
+                    'exec /usr/bin/docker-entrypoint.sh minio server --console-address :9001 /data'],
+                'volumes': [str(minio_storage_dir.absolute()) + ':/data/'],
+                'healthcheck': {'test': ['CMD',
+                                         'curl',
+                                         '-f',
+                                         f"{connection_info['host']}/minio/health/live"],
+                                'interval': '30s',
+                                'timeout': '20s',
+                                'retries': 3},
+                'user': f"{os.geteuid()}:{os.getegid()}"
+            }
+        }
+    }
+    return docker_compose
+
+
+def minio_wait(minio_url: str, num_retries: int=10, sleep_time: int=5) -> bool:
+    if num_retries <= 0:
+        raise ValueError(f"num_retries must be > 0 (got {num_retries})")
+    if sleep_time <= 0:
+        raise ValueError(f"sleep_time must be > 0 (got {sleep_time})")
+
+    minio_check_url = f"{minio_url}/minio/health/live"
+    for i in range(num_retries):
+        try:
+            with urlopen(minio_check_url) as response:
+                if 200 <= response.status < 300:
+                    _log.info("Minio seems ok!")
+                    return True
+        except (URLError, ConnectionError) as e:
+            _log.debug("Attempting to connect to minio generated %s", e)
+        _log.debug("Minio isn't answering yet")
+        if i < num_retries - 1:
+            time.sleep(sleep_time)
+    return False
